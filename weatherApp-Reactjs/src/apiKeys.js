5c01204ac247c43a8f142889c45769e6b36d14bf
Tanishk <tanishk15112001@gmail.com>
2024-08-25 04:11:41 +0530

    Final Commit

---
+++ weatherApp-Reactjs/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "f477e7a11b06f6204c5192ad6482ad76",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
